506882f9a28a629862b11af7010c7e42d402d0d0
Thomas Beckenhauer <thomas.beckenhauer.developer@gmail.com>
2021-06-16 20:39:08 -0400

    remove unneeded files

--- startCudaUbuntu.sh
+++
@@ -1,2 +0,0 @@
-
-docker run -it -v ~/workspace:/workspace "${perm_conf[@]}" --runtime=nvidia nvidia/cuda:11.0.3-runtime-ubuntu18.04 bash
--- startTensorflowDocker.sh
+++
@@ -1,2 +0,0 @@
-
-docker run -it -v ~/workspace:/workspace "${perm_conf[@]}" --runtime=nvidia tensorflow/tensorflow:2.3.1-gpu bash
