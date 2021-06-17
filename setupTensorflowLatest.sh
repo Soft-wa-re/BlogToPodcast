2d1481e65d4d56ba72cdc62baadf83efe31c21c6
Thomas Beckenhauer <thomas.beckenhauer.developer@gmail.com>
2021-06-16 21:33:23 -0400

    updates

--- setupTensorflowLatest.sh
+++ setupTensorflowLatest.sh
@@ -1,11 +1,8 @@
 apt update
-apt install -y vim libsndfile1-dev zsh tmux wget git libsndfile1
-apt install ffmpeg
+apt install -y vim libsndfile1-dev zsh tmux wget git libsndfile1 ffmpeg
 /usr/bin/python3 -m pip install --upgrade pip
 
-pip install soundfile
-pip install numpy
+pip install soundfile numpy pydub
 pip install git+https://github.com/repodiac/german_transliterate.git#egg=german_transliterate
 pip install git+https://github.com/TensorSpeech/TensorflowTTS.git
-pip install pydub
 
--- startTensorflowLatest.sh
+++ startTensorflowLatest.sh
@@ -1,6 +1,4 @@
+#docker run -it -v ~/workspace:/workspace "${perm_conf[@]}" --runtime=nvidia tensorflow/tensorflow:latest-gpu bash
 
+docker run -it -v ~/workspace:/workspace "${perm_conf[@]}" --runtime=nvidia tensorflow/tensorflow:2.3.1-gpu bash
 
-docker run -it -v ~/workspace:/workspace "${perm_conf[@]}" --runtime=nvidia tensorflow/tensorflow:latest-gpu bash
-#docker run -it -v ~/workspace:/workspace "${perm_conf[@]}" tensorflow/tensorflow bash
-
-#--runtime=nvidia tensorflow/tensorflow:2.3.1-gpu bash
