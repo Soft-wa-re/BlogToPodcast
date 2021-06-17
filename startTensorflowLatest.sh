aea77fe6f91e2a9b74072497ebfae70b8c42226d
Thomas Beckenhauer <thomas.beckenhauer.developer@gmail.com>
2021-06-16 21:45:47 -0400

    updates

--- startTensorflowLatest.sh
+++ startTensorflowLatest.sh
@@ -1,4 +1,8 @@
 #docker run -it -v ~/workspace:/workspace "${perm_conf[@]}" --runtime=nvidia tensorflow/tensorflow:latest-gpu bash
 
-docker run -it -v ~/workspace:/workspace "${perm_conf[@]}" --runtime=nvidia tensorflow/tensorflow:2.3.1-gpu bash
+#docker run -it -v ~/workspace:/workspace "${perm_conf[@]}" --runtime=nvidia tensorflow/tensorflow:2.3.1-gpu bash
+
+docker run -it -v ~/workspace:/workspace "${perm_conf[@]}" --runtime=nvidia 9fb490a67746 bash
+
+
 
