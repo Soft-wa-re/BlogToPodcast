08072eec9273b132796ebc9aa51902bac1d015a0
Thomas Beckenhauer <tom@beckenhauer.me>
2021-09-21 07:37:59 -0500

    add python shebangs, make scripts executable.

--- buildDocker.py
+++ buildDocker.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python3
+
 import os
 
 os.system('docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) -t blog_to_podcast .')
--- test/runBlogCastDebug.py
+++ test/runBlogCastDebug.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python3
+
 import os
 
 os.system('docker run --rm -it  -v "$(pwd):/home/testuser/workspace/InputProject" -v "$(pwd)/../Blog_BlogToPodcast:/home/testuser/workspace/BlogCast" blog_to_podcast bash')
