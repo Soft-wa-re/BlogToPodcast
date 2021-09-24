3386ef2579d238163fa53738642f40b360b95f84
Tom Beckenhauer <tbeckenhauer@users.noreply.github.com>
2021-09-24 16:32:03 -0500

    Create manual.yml
    Create manual.yml
    
    Update manual.yml
    
    Update manual.yml
    
    Update manual.yml
    troubleshoot
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    debug
    
    clean out unnecessary file
    
    push to docker
    
    disable dry run

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,2 +1,6 @@
 docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) \
- -t blog_to_podcast .
+ -t tmbecken/blog_to_podcast .
+
+docker login -u tmbecken -p "6o*1FAxGN"
+
+docker push tmbecken/blog_to_podcast
