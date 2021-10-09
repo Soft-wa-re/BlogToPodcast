7fa9eb5d3b7b8b5f146141d442a3996bfc64587c
Tom Beckenhauer <tbeckenhauer@users.noreply.github.com>
2021-10-09 15:03:42 -0400

    tryout actions secrets

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,6 +1,6 @@
 podman build --build-arg UID=$(id -u) --build-arg GID=$(id -g) \
  -t tmbecken/blog_to_podcast .
 
-podman login -u tmbecken -p "6o*1FAxGN"
+podman login -u tmbecken -p $DOCKER_PASSWORD
 
 podman push tmbecken/blog_to_podcast
