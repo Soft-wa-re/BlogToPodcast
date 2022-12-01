ab1fa6192f07b800747d11ae7f43029584cf2f99
Thomas Beckenhauer <thomas.beckenhauer.developer@gmail.com>
2022-12-01 07:01:29 -0500

    build patches

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,2 +1,7 @@
-docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) \
- -t blog_to_podcast .
+cd $( dirname -- "$0" )
+
+echo $(pwd)
+
+docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) -t blog_to_podcast .
+
+cd -
