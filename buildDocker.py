f1688edde3dbe55bf524e6b2853f69d047e4b82c
Tom Beckenhauer <tbeckenhauer@users.noreply.github.com>
2022-12-11 22:09:09 -0500

    Should be able to roll back.

--- buildDocker.py
+++ buildDocker.py
@@ -1,11 +1,5 @@
 #!/usr/local/bin/python3
 
-<<<<<<< HEAD
-import subprocess
-
-subprocess.run("docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) -t blog_to_podcast .", shell=True, check=True)
-=======
 import os
 
 os.system('docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) -t blog_to_podcast .')
->>>>>>> da0541c21b6b98423ac955e2da68022d93825967
