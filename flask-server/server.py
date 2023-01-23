b2ac93e5711f9f4ac49dec33445936e3922964cf
Thomas Beckenhauer <thomas.beckenhauer.developer@gmail.com>
2023-01-23 08:47:21 -0500

    Switch vercel over to flask demo

--- flask-server/server.py
+++ flask-server/server.py
@@ -3,11 +3,20 @@ from flask import Flask
 app = Flask(__name__)
 
 
-# Members API Route
+@app.route('/')
+def home():
+  return 'Hello, Plogcastly!'
+
+
+@app.route('/about')
+def about():
+  return 'About'
+
+
 @app.route("/members")
 def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+  return {"members": ["Member1", "Member2", "Member3"]}
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
