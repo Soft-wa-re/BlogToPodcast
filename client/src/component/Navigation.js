aa42beb14bfa8378220c938fc437c29323c162e2
Thomas Beckenhauer <thomas.beckenhauer.developer@gmail.com>
2023-02-26 14:08:55 -0500

    hide junk

--- client/src/component/Navigation.js
+++ client/src/component/Navigation.js
@@ -7,12 +7,6 @@ import {
 function Navigation () {
 	return (
         <nav>
-//          <Link
-//              className="App-link"
-//              to="/">App</Link>
-//          <Link
-//              className="App-link"
-//              to="/about">About Us</Link>
         </nav>
 	)
 }
